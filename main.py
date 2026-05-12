5e7912e29b8793a553f119411cf0e753ab2bb245
simcha <simchacop@gmail.com>
2026-05-12 17:35:52 +0300

    feat: func 'choose random word' for the word to guess, using import random, randint for random word

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+from random import randint
+
+
 def display_menu():
     message = "GUESSES GAME/n" \
     "You should guess the letters to fill all empty places in the word." \
     "You have limited number of rong guesses" 
     print(message)
 
+
 def is_user_start_playing():
     message = "If you want to play, click 'Y', else click 'N': "
     choice = input(message).upper() == "Y"
     return choice
 
+
 def display_goodby():
     message = "See you next time!"
     print(message)
+
 
 WORDS_TO_GUESS = [
     "Apple",
@@ -25,6 +31,11 @@ WORDS_TO_GUESS = [
     "Cloud",
     "Bread"
 ]
+
+
+def choose_random_word():
+    i = randint(0, 9)
+    return WORDS_TO_GUESS[i]
 
 
 
